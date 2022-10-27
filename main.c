147a50cf97fbfc157a38e38c84494f0a8481e196
Mehmet Tezel <56451989+mehmet-tezel@users.noreply.github.com>
2022-10-27 23:52:26 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 int main(void) {
 
     Node* root = NULL;
-    root = list_ordered_push_back(root, 1);
-    root = list_ordered_push_back(root, 15);
-    root = list_ordered_push_back(root, 5);
-    root = list_ordered_push_back(root, 43);
-    root = list_ordered_push_back(root, 2);
-    root = list_ordered_push_back(root, 9);
-    root = list_ordered_push_back(root, 400);
-    root = list_ordered_push_back(root, 5);
+    root = list_ordered_push(root, 1);
+    root = list_ordered_push(root, 15);
+    root = list_ordered_push(root, 5);
+    root = list_ordered_push(root, 43);
+    root = list_ordered_push(root, 2);
+    root = list_ordered_push(root, 9);
+    root = list_ordered_push(root, 400);
+    root = list_ordered_push(root, 5);
     list_print(root);
 
     list_insert(root, 5, 99999);
@@ -20,10 +20,10 @@ int main(void) {
     printf("root_1 size: %d\n", list_size(root));
 
     Node* root2 = NULL;
-    root2 = list_push_back(root2, 99);
-    root2 = list_push_back(root2, 99);
-    root2 = list_push_back(root2, 54);
-    root2 = list_push_back(root2, 989848994);
+    root2 = list_push(root2, 99);
+    root2 = list_push(root2, 99);
+    root2 = list_push(root2, 54);
+    root2 = list_push(root2, 989848994);
     root2 = list_remove(root2, 99);
     root2 = list_remove_idx(root2, 1);
     list_print(root2);
